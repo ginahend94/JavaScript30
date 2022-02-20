4322dd71bfcdb6c3e5b0a599566daf44b7138195
Gina Henderson <gina@ginahenderson.me>
2022-02-20 01:48:44 -0600

    Identify key pressed

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,3 +1,10 @@
 // Add class on keydown
+const keys = document.querySelectorAll('.key');
+document.addEventListener('keydown', addPlayingClass)
+// keys.forEach(key => key.addEventListener('click', addPlayingClass))
+
+function addPlayingClass(e) {
+    alert(e.keyCode);
+}
 // Play audio clip
 // Remove class on keyup
